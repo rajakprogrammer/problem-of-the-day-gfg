d77fa02c9a55146392b7232cf183dd5a091c8960
Sanjay Baitha <91378949+rajakprogrammer@users.noreply.github.com>
2025-04-01 21:48:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/DFS of Graph/dfs-of-graph.cpp
@@ -0,0 +1,77 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    void dfsUtil(int node, vector<vector<int>>& adj, vector<bool>& visited, vector<int>& result) {
+        visited[node] = true;
+        result.push_back(node);
+        
+        for (int neighbor : adj[node]) {
+            if (!visited[neighbor]) {
+                dfsUtil(neighbor, adj, visited, result);
+            }
+        }
+    }
+    
+    vector<int> dfs(vector<vector<int>>& adj) {
+        int V = adj.size(); // Number of vertices
+        vector<bool> visited(V, false);
+        vector<int> result;
+        
+        // Assuming DFS starts from node 0
+        for (int i = 0; i < V; i++) {
+            if (!visited[i]) {
+                dfsUtil(i, adj, visited, result);
+            }
+        }
+        
+        return result;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int tc;
+    cin >> tc;
+    cin.ignore();
+    while (tc--) {
+        int V;
+        cin >> V;
+        cin.ignore();
+        vector<vector<int>> adj(
+            V); // Use vector of vectors instead of array of vectors.
+
+        for (int i = 0; i < V; i++) {
+            string input;
+            getline(cin, input);
+            int num;
+            vector<int> node;
+            stringstream ss(input);
+            while (ss >> num) {
+                node.push_back(num);
+            }
+            adj[i] = node;
+        }
+
+        Solution obj;
+        vector<int> ans = obj.dfs(adj);
+        for (int i = 0; i < ans.size(); i++) {
+            cout << ans[i] << " ";
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
